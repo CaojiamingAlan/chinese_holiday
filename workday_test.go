4e96a5319c31afc2e336e231cd39177108e4b767
caojiaming.1997 <caojiaming.1997@bytedance.com>
2021-10-28 14:55:00 +0800

    add holiday

--- workday_test.go
+++ workday_test.go
@@ -8,6 +8,11 @@ func TestIsWorkday(t *testing.T) {
 		t.Fatal()
 	}
 
+	//2021-10-01 is a holiday
+	if IsWorkday("2021-10-01") == true {
+		t.Fatal()
+	}
+
 	//2021-10-29 is Friday
 	if IsWorkday("2021-10-29") == false {
 		t.Fatal()
